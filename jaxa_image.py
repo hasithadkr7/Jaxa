f160d19073bd282efaba4b55002e3b33276ebdcc
hasitha <hasithadkr7@gmail.com>
2019-10-13 06:37:45 +0530

    generate file time between given time gap

--- jaxa_image.py
+++ jaxa_image.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import os
 from urllib.request import urlopen
 import glob
@@ -18,8 +18,8 @@ def format_jaxa_df(df,
 
 
 def create_png(d03_df,
-               base_image_path='/home/hasitha/PycharmProjects/DSS-Framework/jupyter_notebooks/jaxa_data/map.png',
-               output_image_path='/home/hasitha/PycharmProjects/DSS-Framework/jupyter_notebooks/jaxa_data/output.png',
+               base_image_path='/home/hasitha/PycharmProjects/Jaxa/input/map.png',
+               output_image_path='/home/hasitha/PycharmProjects/Jaxa/output/output.png',
                image_extent=[79.55, 82.15, 5.75, 10.05]):
     lanka_img = mpimg.imread(base_image_path)
     ax = d03_df.plot(kind="scatter", x="Lon", y="Lat", figsize=(12, 10),
@@ -40,7 +40,7 @@ def create_png(d03_df,
     plt.savefig(output_image_path)
 
 
-def unzip_file(src, dest='/home/hasitha/PycharmProjects/DSS-Framework/jupyter_notebooks/jaxa_data'):
+def unzip_file(src, dest='/home/hasitha/PycharmProjects/Jaxa/output'):
     with zipfile.ZipFile(src, 'r') as zip_ref:
         zip_ref.extractall(dest)
 
@@ -69,4 +69,27 @@ def download_jaxa_data(date_str, dir_path=None):
     download_file(jaxa_url, jaxa_zip_file)
     return jaxa_zip_file
 
+
+def download_jaxa_data_for_given_time_frame(start_time, end_time, time_step=30, time_gap=59,
+                                            dir_path='/home/hasitha/PycharmProjects/Jaxa/input'):
+    """
+    :param start_time: str '2019-10-12 08:00:00'
+    :param end_time: str '2019-10-12 18:00:00'
+    :param dir_path:
+    :return:
+    """
+    start_time = datetime.strptime(start_time, '%Y-%m-%d %H:%M:%S')
+    end_time = datetime.strptime(end_time, '%Y-%m-%d %H:%M:%S')
+    while start_time < end_time:
+        time1 = start_time
+        time2 = start_time + timedelta(minutes=time_gap)
+        time1 = time1.strftime('%Y-%m-%d %H:%M:%S')
+        time2 = time2.strftime('%Y-%m-%d %H:%M:%S')
+        print('[time1 , time2] : ', [time1 , time2])
+        start_time = start_time + timedelta(minutes=time_step)
+
+
+if __name__ == '__main__':
+    download_jaxa_data_for_given_time_frame('2019-10-12 08:00:00',
+                                            '2019-10-13 10:00:00')
 
